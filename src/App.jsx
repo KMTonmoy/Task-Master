a78e14bf89116517c3fe6775a201859d90c71d65
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-11-15 10:15:32 +0600

    UI Some  Structure Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './App.css'
 const App = () => {
+
+  const addTask = () => {
+
+    console.log("New Task Added 😜😜")
+
+  }
+
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='container'>
+      <div className='todo-app'>
+        <h2>My To-Do List</h2>
+        <div className="status">
+          <p>Total Tasks:0</p>
+          <p>
+            Completed Tasks: 0
+          </p>
+        </div>
+        <div className="row">
+          <input
+            type="text"
+            placeholder="Add a new task"
+          />
+          <button className="add-task-button" onClick={addTask}>
+            Add Task
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
